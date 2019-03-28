399d8b581541b1882beaef1971fafd53e86ff8be
Victor Barbosa <freitasdfb@gmail.com>
2019-03-28 14:56:28 -0300

    Commit inicial com início da implementação do framework

---
+++ config/database.js
@@ -0,0 +1,7 @@
+module.exports = {
+  username: 'victor',
+  password: 'victor',
+  database: 'digitalpresenca',
+  host: '127.0.0.1',
+  dialect: 'mariadb',
+};
---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nunjucks = require('nunjucks');
+const path = require('path');
+const routes = require('./app/routes');
+
+const app = express();
+
+app.use(express.static(path.resolve('app', 'public')));
+
+app.listen(3000, () => {
+  console.log('Server iniciado na porta 3000');
+});
