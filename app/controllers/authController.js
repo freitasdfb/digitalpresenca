57c406f51aeecf8f6933288aa13d958e4ed1552e
Victor Barbosa <freitasdfb@gmail.com>
2019-03-28 17:56:55 -0300

    Alert cpf already exists

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -15,12 +15,14 @@ module.exports = {
     const { cpf } = req.body;
 
     if (await User.findOne({ where: { cpf } })) {
+      req.flash('error', 'CPF já cadastrado');
       return res.redirect('back');
     }
 
     const password = await bcrypt.hash(req.body.password, 5);
 
     await User.create({ ...req.body, password });
+    req.flash('success', 'Cadastro realizado com sucesso');
     return res.redirect('/');
   },
 };
---
+++ config/session.js
@@ -0,0 +1,5 @@
+module.exports = {
+  secret: 'digitalpresenca',
+  resave: false,
+  saveUninitialized: true,
+};
